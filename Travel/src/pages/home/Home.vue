b4e3c8712e296bd0a3a767ad1cf7bd43a3963070
eugeneblog <714284150@qq.com>
2018-08-22 19:28:25 +0800

    initPro

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>Home</div>
+    <router-link to="/list" class="home">list</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .home{
+    font-size: 50px;
+  }
+</style>
---
+++ Travel/src/pages/list/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>list</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
