88ee5042d98f0a28b4ea25ec698fe1b8c787527e
eugeneblog <714284150@qq.com>
2018-08-28 15:10:42 +0800

    finish city css

---
+++ Travel/src/pages/city/components/List.vue
@@ -0,0 +1,183 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="buttom-list">
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="buttom-list">
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                    <div class="buttom-wraper">
+                        <div class="buttom">
+                            北京
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                    <div class="item border-bottom">
+                        阿拉尔
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'   //页面滚动插件
+export default {
+    name: "CityList",
+    mounted() {
+        this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import "~styles/varibles.styl";
+    .border-topbottom
+        &:before
+            border-color #777
+        &:after
+            border-color #777
+    .border-bottom
+        &:before
+            border-color #777
+    .list
+        position absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        overflow hidden
+        .title
+            line-height .54rem
+            background #eeeeee
+            padding-left .2rem
+            color #666
+            font-size .26rem
+        .buttom-list
+            overflow hidden
+            padding .1rem .6rem .1rem .1rem
+            .buttom-wraper
+                float left
+                width 33.3%
+                .buttom
+                    margin .1rem
+                    padding .1rem 0
+                    border .02rem solid #cccccc
+                    border-radius .06rem
+                    text-align center
+        .item-list
+            .item
+                line-height .54rem
+                color #666666
+                padding-left .2rem
+</style>
