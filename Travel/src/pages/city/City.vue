64b25ae96973d211d1ba4d206167728df21aa0ec
eugeneblog <714284150@qq.com>
2018-08-31 00:59:34 +0800

    add search-logic

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <city-header></city-header>
-        <city-search></city-search>
+        <city-search :cities = 'cities'></city-search>
         <city-list :cities = "cities" :hot = "hotCities" :letter = "letter"></city-list>
         <city-alphabet :cities = "cities" @change="handleLetterChange"></city-alphabet>
     </div>
@@ -24,8 +24,7 @@ export default {
     },
     data() {
         return {
-            cities: {
-            },
+            cities: {},
             hotCities: [],
             letter: ''
         }
